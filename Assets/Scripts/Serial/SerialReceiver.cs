1679e0a7cdb2b81722e0eced6793642cced8e214
Ryoma Saito(Dedicated School Account) <96648305+saitocastel1900@users.noreply.github.com>
2024-06-23 23:19:37 +0900

    ファイルの再配置

---
+++ Assets/Scripts/Serial/SerialReceiver.cs
@@ -0,0 +1,24 @@
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class SerialReceiver : MonoBehaviour
+{
+    [Inject] private IInputEventProvider _input;
+    [SerializeField] private GameObject _object;
+
+    private void Start()
+    {
+        _input.InclinationAmount.Subscribe(Rotate).AddTo(this.gameObject);
+    }
+
+    private void Rotate(Vector3 acceleration)
+    {
+        float pitch = Mathf.Atan2(acceleration.y,
+            Mathf.Sqrt(acceleration.x * acceleration.x + acceleration.z * acceleration.z)) * Mathf.Rad2Deg;
+        float roll = Mathf.Atan2(-acceleration.x, acceleration.z) * Mathf.Rad2Deg;
+
+        var targetRotation = Quaternion.Euler(pitch, 0, roll);
+        _object.transform.rotation = Quaternion.Lerp(_object.transform.rotation, targetRotation, .25f);
+    }
+}
